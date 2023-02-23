189e500083f8fef972e587356c89e6e1dbc0db7d
Eruoti1 <kevinombui49@gmail.com>
2023-02-22 17:41:38 -0800

    file 6

--- errors1.c
+++ errors1.c
@@ -6,6 +6,7 @@
  * Return: 0 if no numbers in string, converted number otherwise
  *       -1 on error
  */
+
 int _erratoi(char *s)
 {
 	int i = 0;
@@ -35,6 +36,7 @@ int _erratoi(char *s)
  * Return: 0 if no numbers in string, converted number otherwise
  *        -1 on error
  */
+
 void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
@@ -53,6 +55,7 @@ void print_error(info_t *info, char *estr)
  *
  * Return: number of characters printed
  */
+
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
@@ -93,6 +96,7 @@ int print_d(int input, int fd)
  *
  * Return: string
  */
+
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
@@ -127,6 +131,7 @@ char *convert_number(long int num, int base, int flags)
  *
  * Return: Always 0;
  */
+
 void remove_comments(char *buf)
 {
 	int i;
