884a6ccfdab87faaaeb200836d43dadfb1d5bbe2
Eruoti1 <kevinombui49@gmail.com>
2023-02-22 17:43:21 -0800

    file 7

--- exits.c
+++ exits.c
@@ -7,6 +7,7 @@
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
@@ -37,6 +38,7 @@ char *_strncpy(char *dest, char *src, int n)
  *@n: the amount of bytes to be maximally used
  *Return: the concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
@@ -63,6 +65,7 @@ char *_strncat(char *dest, char *src, int n)
  *@c: the character to look for
  *Return: (s) a pointer to the memory area s
  */
+
 char *_strchr(char *s, char c)
 {
 	do {
